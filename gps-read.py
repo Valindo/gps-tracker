72d1f8310251f1018da96383ace7a81252d3e993
Valindo Godinho <valindo.godinho@gmail.com>
2018-03-28 15:19:06 +0530

    Done with parsing gps data and sending it over mqtt

--- gps-read.py
+++ gps-read.py
@@ -1,7 +1,11 @@
 import serial
 import time
+import paho.mqtt.client as mqtt
+import json
 
 # gps = serial.Serial('COM3',4800,timeout=0)
+client = mqtt.Client()
+client.connect("iot.eclipse.org", 1883, 60)
 
 def find_lat(temp):
     return temp.split(',')[3]
@@ -14,7 +18,10 @@ while(1):
     # temp = gps.readline()
     temp = "$GPRMC,113209.000,A,1529.0377,N,07348.8097,E,0.66,262.34,240318,,,A*69"
     if temp[0:6] == "$GPRMC":
-        print find_lat(temp)
-        print find_long(temp)
-        print '\n\n'
+        #Do the calculation for deg to decimal
+        lat =  find_lat(temp)
+        #Do the calculation for deg to decimal
+        longi =  find_long(temp)
+        data = { "lat": lat, "long": longi}
+        client.publish('collegetest/raspberry', json.dumps(data), 0, False)
     time.sleep(.500)
